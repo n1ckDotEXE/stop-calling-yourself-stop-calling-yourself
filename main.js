7e2ed6bc593a53a63895179a53e6475ccfd10f30
n1ckDotEXE <admin@nicholasnam.com>
2020-09-22 14:39:22 -0400

    Completed

---
+++ main.js
@@ -0,0 +1,34 @@
+// add three question marks to the string
+
+function triQuestionAdder(param1) {
+    return param1 + '???';
+}
+
+// add two strings with a space in the middle
+
+function stringCombinerThingyWithSpaceBonus(param1, param2) {
+    return param1 + ' ' + param2;
+}
+
+// subtract three numbers
+
+function subtractionFunctionThatSubtractsThreeNumbers(param1, param2, param3) {
+    return param1 - param2 - param3;
+}
+
+// subtract three numbers in reverse order
+
+function subtractionFunctionThatSubtractsThreeNumbersButBackwardsThough (param1, param2, param3) {
+    return param3 - param2 - param1;
+}
+
+// triple a string
+
+function stringTripler(param1) {
+    return param1 + param1 + param1;
+}
+
+// return the square root of a number (you'll probably need research!)
+function doSquareRootAfterResearch(param1) {
+    return param1.sqrt();
+}
